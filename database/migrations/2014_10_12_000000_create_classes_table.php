0920342b45cee5a7e15de9b40d0e8dbf33c22d0a
lincanbin <lincanbin@hotmail.com>
2016-01-21 11:37:21 +0800

    Add classes table.

---
+++ database/migrations/2014_10_12_000000_create_classes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('classnumber');
+            $table->string('name');
+            $table->string('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('classes');
+    }
+}
