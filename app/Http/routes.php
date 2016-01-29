fb9055ad21020df610baff92c37ccc1e6cd11bca
lincanbin <lincanbin@hotmail.com>
2016-01-29 20:46:45 +0800

    Modify routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,12 +27,8 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
-});
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
-    Route::get('/home', 'HomeController@index');
+    Route::get('/', 'HomeController@index');
 });
