750f6787fa7144546ffb58e3bf1db68c0ec5b10b
lincanbin <lincanbin@hotmail.com>
2016-01-30 18:03:07 +0800

    Make the tree of classes.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,6 +29,10 @@ class HomeController extends Controller
             FROM `classes` 
             WHERE char_length(classnumber)<=3 
                   AND char_length(classnumber)>=1');
-        return view('home', ['classes' => $classes]);
+        $classes_tree = array();
+        foreach ($classes as $class) {
+            $classes_tree[substr($class->classnumber, 0, 1)][substr($class->classnumber, 0, 2)][$class->classnumber][] = $class->name;
+        }
+        return view('home', ['classes_tree' => $classes_tree]);
     }
 }
