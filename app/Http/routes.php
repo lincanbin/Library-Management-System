ffee78b7e8e9b215a1890f1e5392b1d09d25d618
lincanbin <lincanbin@hotmail.com>
2016-01-30 21:30:51 +0800

    Delete useless route.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,11 +10,11 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 
 /*
 |--------------------------------------------------------------------------
