86598f90aa72d3516c992baf36e9e7e389eb6ce9
lincanbin <lincanbin@hotmail.com>
2016-03-19 17:16:40 +0800

    SearchController done.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -18,75 +18,76 @@ class SearchController extends Controller
         //$this->middleware('auth');
     }
     public function category($category, $page){
-        return $this->core($category, '', intval($page));
+        return $this->core($category, null, intval($page));
     }
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    public function search($keyword, $page){
+        return $this->core(null, $keyword, intval($page));
+    }
+
     public function core($category, $keyword, $page)
     {
-        $list_show_num = 20;
-        /*
-        $classes = DB::select('SELECT name, classnumber 
-            FROM `classes` 
-            WHERE char_length(classnumber)<=3 
-                  AND char_length(classnumber)>=1');
-        $classes_tree = array();
-        foreach ($classes as $class) {
-            $classes_tree[substr($class->classnumber, 0, 1)][substr($class->classnumber, 0, 2)][$class->classnumber][] = $class->name;
-        }
-        //return view('home', ['classes_tree' => $classes_tree]);
-        */
-        
-
+        $list_show_num = 18;//每页显示的条目数量
+        $category_info = array();
         if ( $keyword ) {
-            $keyword_arr = explode(" ", $keyword);
-            $keyword_sql = " MATCH (name,author,d_tags) AGAINST (? IN BOOLEAN MODE)";
+            //$keyword_arr = explode(" ", $keyword);
+            $append_sql = " MATCH (name, author, d_tags) AGAINST (? IN BOOLEAN MODE)";
             $parameter = $keyword;
             $title_short = "搜索包含 " . $keyword . " 的图书";
         }
         if ( $category ) {
             //获取分类信息（中图法）
-            $query_sql = "SELECT name,content FROM `classes` WHERE classnumber = ?";
-            $category_arr = get_object_vars(DB::select($query_sql, [$category])[0]);
-            var_dump($category_arr);
+            $query_sql = "
+                SELECT name, content 
+                FROM `classes` 
+                WHERE classnumber = ?";
+            $category_info = get_object_vars(DB::select($query_sql, [$category])[0]);
+            //var_dump($category_info);
 
             $append_sql = "classnumber like ?";
             $parameter = $category.'%';
-            $title_short = "".$category_arr['name']."(分类号：".$category.")";
+            $title_short = "".$category_info['name']."(分类号：".$category.")";
         }
-        $c_num = intval(get_object_vars(DB::select("SELECT count(id) FROM `books` WHERE " . $append_sql, [$parameter])[0])['count(id)']);
-        print_r($c_num);
+        $result_num = intval(get_object_vars(DB::select("
+            SELECT count(id) 
+            FROM `books` 
+            WHERE " . $append_sql, [$parameter])[0])['count(id)']);
+        //print_r($result_num);
         
-        if ($c_num == 0) {
+        if ($result_num == 0) {
             exit('查无搜索结果');
         }
 
         // 处理正确的页数
-        $taltol_page = ceil($c_num / $list_show_num);
+        $total_page = ceil($result_num / $list_show_num);
         if ($page < 1) {
             exit('404');
-        } else if ($page > $taltol_page) {
+        } else if ($page > $total_page) {
             exit('404');
         }
-        
-        
-        
+
         // 获取所需的图书列表
         if ($page == 0)
             $page = 1;
-        $query_sql = "SELECT id,name,author,image,get_id,publisher,rating 
+        $query_sql = "SELECT id, name, author, image, get_id, publisher, rating 
             FROM `books` 
             WHERE " . $append_sql . " 
             ORDER BY rating DESC,id DESC 
             LIMIT " . ($page - 1) * $list_show_num . "," . $list_show_num;
         $books     = DB::select($query_sql, [$parameter]);
-        print_r($books);
+        //print_r($books);
 
         // 页面变量
-        $title = $title_short." - 暨南大学图书馆 - page " . $page;
-        return $category. 'test';
+        $title = $title_short." - page " . $page;
+        return view('search', [
+            'title' => $title, 
+            'keyword' => $keyword,
+            'category' => $category,
+            'books' => $books,
+            'category_info' => $category_info,
+            'page' => $page,
+            'total_page' => $total_page,
+            'result_num' => $result_num
+        ]);
     }
 }
