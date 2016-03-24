c0c3643b4bb9fef836dba49ca62cc4a2785ad551
lincanbin <lincanbin@hotmail.com>
2016-03-25 01:38:48 +0800

    Add new table: records.

---
+++ database/migrations/2014_10_12_000000_create_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('book_id');
+            $table->integer('time');
+            $table->integer('return_time');
+            $table->boolean('enable');
+
+            $table->index('user_id');
+            $table->index('return_time');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('records');
+    }
+}
