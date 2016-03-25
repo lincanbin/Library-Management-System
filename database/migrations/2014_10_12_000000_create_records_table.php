335f6e54eadd55dd4c1858a86789e3576c6f7d5c
lincanbin <lincanbin@hotmail.com>
2016-03-26 01:58:41 +0800

    Fixed bug of Migrations.

--- database/migrations/2014_10_12_000000_create_records_table.php
+++ database/migrations/2014_10_12_000000_create_records_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBooksTable extends Migration
+class CreateRecordsTable extends Migration
 {
     /**
      * Run the migrations.
