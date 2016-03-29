4b90c2ad83c866323991289f250c87fd20972778
lincanbin <lincanbin@hotmail.com>
2016-03-30 01:16:04 +0800

    Add ManageController.

---
+++ app/Http/Controllers/ManageController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ManageController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $records = DB::table('records')->orderBy('return_time')->get();
+        return view('manage', ['records' => $records]);
+    }
+
+    public function store(Request $request)
+    {
+        if (Auth::check()){
+            DB::table('records')->insert([
+                'id' => null,
+                'user_id' => intval(Auth::user()->id),
+                'book_id' => intval($request->input('id')),
+                'time' => $_SERVER['REQUEST_TIME'],
+                'return_time' => $_SERVER['REQUEST_TIME']+86400*30,
+                'enable' => 0//是否允许外借，默认为0，不允许
+            ]);
+            $result=[
+                'status' => 1
+            ];
+        }else{
+            $result=[
+                'status' => 0
+            ];
+        }
+        return json_encode($result);
+    }
+
+    public function update($id)
+    {
+        DB::table('records')
+            ->where('id', intval($id))
+            ->update(['enable' => 1]);
+        $result=[
+            'status' => 1
+        ];
+        return json_encode($result);
+    }
+}
