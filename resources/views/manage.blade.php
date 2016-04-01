ab2f7289cc1c0de27a6a233a147d0a0e59117d30
lincanbin <lincanbin@hotmail.com>
2016-04-02 02:01:35 +0800

    Show the records of library.

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -2,20 +2,28 @@
 @section('title', '借书管理')
 @section('content')
 <section class="section--center mdl-grid mdl-grid--no-spacing">
-<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+<table class="mdl-data-table mdl-js-data-table mdl-data-table mdl-shadow--2dp">
   <thead>
     <tr>
-      <th class="mdl-data-table__cell--non-numeric">Material</th>
-      <th>Quantity</th>
-      <th>Unit price</th>
+      <th class="mdl-data-table__cell--non-numeric">允许借阅</th>
+      <th>书名</th>
+      <th>借阅人</th>
+      <th>借阅时间</th>
+      <th>归还时间</th>
     </tr>
   </thead>
   <tbody>
-    @foreach($records as $key => $record)
+    @foreach($records as $record)
     <tr>
-      <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
-      <td>25</td>
-      <td>$2.90</td>
+      <td>
+        <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-{{ $record->id }}">
+          <input type="checkbox" id="switch-{{ $record->id }}" class="mdl-switch__input" {{ $record->enable==1?'checked="checked"':'' }}>
+        </label>
+      </td>
+      <td class="mdl-data-table__cell--non-numeric">{{ $record->name }}</td>
+      <td>{{ $record->email }}</td>
+      <td>{{ $record->time }}</td>
+      <td>{{ $record->return_time==0?'未归还':$record->return_time }}</td>
     </tr>
     @endforeach
   </tbody>
