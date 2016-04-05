ad278fd35b3eb912b14eaf652196988a920d095c
lincanbin <lincanbin@hotmail.com>
2016-04-05 15:36:06 +0800

    Add more toggle in manage page.

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -10,20 +10,38 @@
       <th>借阅人</th>
       <th>借阅时间</th>
       <th>归还时间</th>
+      <th class="mdl-data-table__cell--non-numeric">归还图书</th>
     </tr>
   </thead>
   <tbody>
     @foreach($records as $record)
     <tr>
       <td>
-        <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-{{ $record->id }}">
-          <input type="checkbox" id="switch-{{ $record->id }}" class="mdl-switch__input" {{ $record->enable==1?'checked="checked"':'' }}>
+        <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="enable-{{ $record->id }}">
+          <input type="checkbox" id="enable-{{ $record->id }}" class="mdl-switch__input" {{ $record->enable==1?'checked="checked"':'' }}>
         </label>
       </td>
-      <td class="mdl-data-table__cell--non-numeric">{{ $record->name }}</td>
-      <td>{{ $record->email }}</td>
-      <td>{{ $record->time }}</td>
-      <td>{{ $record->return_time==0?'未归还':$record->return_time }}</td>
+      <td class="mdl-data-table__cell--non-numeric">{{ $record->book_name }}</td>
+      <td>{{ $record->user_name }}</td>
+      <td>{{ date('Y-m-d H:i', $record->time) }}</td>
+      <td>
+      @if( $record->return_time > 0 )
+          {{ date('Y-m-d H:i', $record->return_time) }}
+      @elseif(($record->time+86400*60) < time())
+           已逾期
+      @elseif(($record->time+86400*30) < time())
+           即将逾期
+      @else
+          未归还
+      @endif
+      </td>
+      <td>
+      @if( $record->return_time == 0 && $record->enable == 1)
+          <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="return-{{ $record->id }}">
+            <input type="checkbox" id="return-{{ $record->id }}" class="mdl-switch__input">
+          </label>
+      @endif
+      </td>
     </tr>
     @endforeach
   </tbody>
