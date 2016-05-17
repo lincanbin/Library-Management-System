8aef47a0080863aa2f2ee60d176f6be6aa4872cf
lincanbin <lincanbin@hotmail.com>
2016-05-18 00:48:52 +0800

    Mobile Chrome support.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#303F9F" />
         <title>
             @yield('title')
         </title>
